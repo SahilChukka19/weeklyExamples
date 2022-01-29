00052672892e302584801ee8ee5863c88b1e5665
Sahil <sahil.chukka@gmail.com>
2022-01-29 19:53:35 +0530

    c program

--- pattern3.c
+++ pattern3.c
@@ -1,5 +1,28 @@
-Write a program to print below triangular pattern:-
-         *
-        ***
-       *****
-      *******
+//Write a program to print below pattern:-
+    //*****
+    //****
+    //***
+    //**
+    //*
+    //**
+    //***
+    //****
+    //*****
+#include<stdio.h>
+int main()
+{
+    #include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=6;i>0;i--)
+    {
+        for(j=i;j>0;j--)
+        {
+        printf("*");
+        }
+    printf("\n");
+    }
+    
+    return 0;
+}
